74c9e2c859c7cf41781fa958a7be9a34d07c3c0f
Ward Bell <wardbell@hotmail.com>
2015-09-22 23:06:00 -0700

    1st unit tests in app-t01

--- TsNorthwind/TsNorthwind/typings/tsd.d.ts
+++ TsNorthwind/TsNorthwind/typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="breeze/breeze.d.ts" />
+/// <reference path="jasmine/jasmine.d.ts" />
