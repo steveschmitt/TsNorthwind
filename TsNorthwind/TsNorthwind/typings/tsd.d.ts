8ee2adcd834723206e8cca35f50c18c311ce2d51
Ward Bell <wardbell@hotmail.com>
2015-09-23 03:34:33 -0700

    app-t02: initial test of an Angular app

--- TsNorthwind/TsNorthwind/typings/tsd.d.ts
+++ TsNorthwind/TsNorthwind/typings/tsd.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="breeze/breeze.d.ts" />
 /// <reference path="jasmine/jasmine.d.ts" />
+/// <reference path="angularjs/angular-mocks.d.ts" />
