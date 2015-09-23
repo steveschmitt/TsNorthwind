afeb6483c57e2fb536dedf2899f975c877519a93
Steve Schmitt <steves@ideablade.com>
2015-09-23 02:30:19 -0500

    Added app-16, beginnings of directive lesson.

---
+++ TsNorthwind/TsNorthwind/app-16/app.module.ts
@@ -0,0 +1,17 @@
+﻿angular.module('app', ['ngRoute']);
+
+angular.module('app').config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/cust', {
+                templateUrl: 'customers.html',
+                controller: 'AppController as vm'
+            }).
+            when('/edit/:id', {
+                templateUrl: 'customer-detail.html',
+                controller: 'EditController as vm'
+            }).
+            otherwise({
+                redirectTo: '/cust'
+            });
+    }]);
---
+++ TsNorthwind/TsNorthwind/app-16/directives.ts
@@ -0,0 +1,14 @@
+﻿namespace app16 {
+
+    angular.module("app").directive('firstdirective', function () {
+        return {
+            template: "<div>This is my directive</div>"
+        }
+    });
+
+    angular.module("app").directive('custeditdirective', function () {
+        return {
+            template: '<div><label>First: </label><input ng-model="vm.customer.firstName" /></div><div><label > Last: </label><input ng-model="vm.customer.lastName" /> </div>'
+        }
+    });
+} 
