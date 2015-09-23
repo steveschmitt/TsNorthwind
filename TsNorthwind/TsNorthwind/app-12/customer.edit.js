b48a99567bc09e20e003add7c9a4ab839890c3d7
Steve Schmitt <steves@ideablade.com>
2015-09-22 23:30:45 -0500

    Adds the EditController, a second controller to use when editing a customer.

---
+++ TsNorthwind/TsNorthwind/app-12/customer.edit.js
@@ -0,0 +1,10 @@
+var app12;
+(function (app12) {
+    var CustomerEdit = (function () {
+        function CustomerEdit(cust) {
+            this.customer = cust;
+        }
+        return CustomerEdit;
+    })();
+})(app12 || (app12 = {}));
+//# sourceMappingURL=customer.edit.js.map
