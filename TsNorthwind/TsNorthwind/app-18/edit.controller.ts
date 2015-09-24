1fb10e0b4233ba93509c78d5f7f30b25ab9aee93
Steve Schmitt <steves@ideablade.com>
2015-09-23 23:55:50 -0500

    Added app-18: pagination and editing a customer (but not saving yet)

---
+++ TsNorthwind/TsNorthwind/app-18/edit.controller.ts
@@ -0,0 +1,27 @@
+﻿namespace app18 {
+    class EditController {
+
+        customer: Customer
+        static $inject = ['BreezeDataservice', 'EditService', '$routeParams', '$location'];
+        constructor(dataService: BreezeDataservice, private editService: EditService, private $routeParams, private $location: ng.ILocationService) {
+            var id = $routeParams.id;
+            this.customer = dataService.getCustomerById(id);
+        }
+
+        ok() { // now "ok", not "save", since we are accepting the change but not saving to server yet.
+            //this.editService.save();
+            this.$location.path('/cust');
+        }
+
+        revert() {
+            // undo any changes
+            var entity = <breeze.Entity><any>this.customer;
+            entity.entityAspect.rejectChanges();
+            entity.entityAspect.entityState.isAddedModifiedOrDeleted()
+            this.$location.path('/cust');
+        }
+    }
+
+    angular.module('app').controller('EditController', EditController);
+
+}
